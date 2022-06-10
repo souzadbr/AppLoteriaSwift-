a40d4a90a2e83fa0ae4e43cf7b613566f489d292
Debora Rodrigues <deboral.p@hotmail.com>
2022-06-10 08:54:20 -0300

    Criar outlet e actions vinculados ao storyboard

--- Loteria/ViewController.swift
+++ Loteria/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scGameType: UISegmentedControl!
+    @IBOutlet var balls: [UIButton]!
+    @IBOutlet weak var lbGameType: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func generateGame(_ sender: Any) {
+    }
 }
 
